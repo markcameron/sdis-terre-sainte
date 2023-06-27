2a2eb8bead0ac5047916d5e4cecfd693194cb4ea
Mark Cameron <mark.cameron@devfactory.ch>
2023-06-27 22:22:28 +0200

    Update package.json and related config files

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./app/View/Components/*.php",
+  ],
+
+  safelist: [
+    {
+        pattern: /grid-cols-(1|2|3|4|6)/,
+        variants: ['sm', 'md', 'lg', 'xl', '2xl'],
+    },
+    {
+        pattern: /text-(black|white|slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900)/,
+    },
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
+        barlow: ['Barlow', ...defaultTheme.fontFamily.sans],
+        barlowCondensed: ['Barlow Condensed', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#E93C3D',
+          dark: '#B51213',
+        },
+        secondary: '#2C75BC',
+        black: '#111',
+      }
+    },
+  },
+
+  plugins: [],
+}
