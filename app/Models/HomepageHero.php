034e61880257432f4af71f77bfe95cffb23e688d
Mark Cameron <mark.cameron@devfactory.ch>
2023-06-29 14:24:15 +0200

    Fix HomepageHero Model config

--- app/Models/HomepageHero.php
+++ app/Models/HomepageHero.php
@@ -5,14 +5,30 @@ namespace App\Models;
 use A17\Twill\Models\Behaviors\HasMedias;
 use A17\Twill\Models\Model;
 
-class HomepageHero extends Model 
+class HomepageHero extends Model
 {
     use HasMedias;
 
     protected $fillable = [
         'published',
         'title',
-        'description',
+        'hero_text',
     ];
-    
+
+    public $mediasParams = [
+        'hero' => [
+            'default' => [
+                [
+                    'name' => 'default',
+                    'ratio' => 16 / 9,
+                ],
+            ],
+            'mobile' => [
+                [
+                    'name' => 'mobile',
+                    'ratio' => 16 / 9,
+                ],
+            ],
+        ],
+    ];
 }
--- resources/views/components/blade/homepage-hero.blade.php
+++ resources/views/components/blade/homepage-hero.blade.php
@@ -1,4 +1,4 @@
-<section class="home-hero__bg px-4 pt-12" style="background-image:url('{{ $hero->image('hero') }}');">
+<section class="home-hero__bg px-4 pt-12" style="background-image:url('{{ $hero->image('hero', 'default') }}');">
     <div class="mx-auto max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl">
         <h1
             class="font-barlow text-center text-2xl uppercase leading-loose text-white md:text-4xl lg:text-5xl xl:text-7xl">
