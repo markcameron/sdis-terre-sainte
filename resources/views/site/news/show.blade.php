cae3a4bf95408b5630c1e304ac69191aa499a064
Mark Cameron <mark.cameron@devfactory.ch>
2023-07-04 22:53:14 +0200

    Fix dates for News detail

--- resources/views/site/news/show.blade.php
+++ resources/views/site/news/show.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     @php
         $image = $news->imageAsArray('cover', 'default', ['w' => 992]);
+        app()->setLocale('fr')
     @endphp
 
     <div class="container max-w-5xl px-4 pb-12 mx-auto">
@@ -13,6 +14,8 @@
                 <section class="container mx-auto mb-4">
                     <h1 class="text-primary text-6xl font-barlowCondensed uppercase">{{ $news->title }}</h1>
                 </section>
+
+                <div class="pb-2">{{ $news->created_at->isoFormat('D MMMM YYYY') }}</div>
 
                 <div class="page-content">
                     <p class="font-semibold">{{ $news->teaser }}</p>
