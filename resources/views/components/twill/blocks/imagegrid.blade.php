7fc9f131e5a975fa71e527d27f7b6b11492787de
Mark Cameron <mark.cameron@devfactory.ch>
2023-07-04 22:42:20 +0200

    Fixes for Imagegrid block to handle columns config

--- resources/views/components/twill/blocks/imagegrid.blade.php
+++ resources/views/components/twill/blocks/imagegrid.blade.php
@@ -3,11 +3,11 @@
     $crops = $block->imagesAsArrays('image', 'desktop', ['w' => 490, 'h' => 367]);
 @endphp
 
-<div class="grid grid-cols-2 gap-4">
+<div class="grid grid-cols-{!! $block->input('columns') !!} gap-4">
 @foreach ($crops as $index => $image)
     <div>
         <a href="{{ $images[$index]['src'] }}">
-            <img src="{{ $image['src'] }}" class="w-full h-auto py-12" alt="{{ $image['alt'] }}" title="{{ $image['caption'] }}">
+            <img src="{{ $image['src'] }}" class="w-full h-auto py-4" alt="{{ $image['alt'] }}" title="{{ $image['caption'] }}">
         </a>
     </div>
 @endforeach
