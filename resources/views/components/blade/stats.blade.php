e8b45bbe60bb348b1de29889d59c5a48dc2f245b
Mark Cameron <mark.cameron@devfactory.ch>
2023-08-10 17:22:46 +0200

    Align stats better on mobile

--- resources/views/components/blade/stats.blade.php
+++ resources/views/components/blade/stats.blade.php
@@ -2,10 +2,11 @@
     <div class="mx-auto max-w-7xl">
         <ul class="grid gap-4 text-white sm:grid-cols-2 md:grid-cols-3 md:text-center">
             @foreach ($stats as $stat)
-                <li class="flex items-center xs:ml-12 gap-16 sm:gap-6 md:flex-col">
-                    <img src="{{ $stat->image('icon') }}" class="w-full max-w-[100px] sm:max-w-[150px] xl:max-w-[200px]"
-                        alt="">
-                    <div class="font-barlowCondensed flex flex-col text-2xl md:items-center xl:text-5xl">
+                <li class="flex items-center xs:ml-12 gap-4 sm:gap-6 md:flex-col">
+                    <div class="flex grow justify-end sm:justify-center">
+                        <img src="{{ $stat->image('icon') }}" class="w-full max-w-[100px] sm:max-w-[150px] xl:max-w-[200px]" alt="">
+                    </div>
+                    <div class="flex grow font-barlowCondensed flex flex-col text-2xl md:items-center xl:text-5xl">
                         <span class="text-5xl xl:text-8xl">{{ $stat->description }}</span>
                         <span>{{ $stat->title }}</span>
                     </div>
